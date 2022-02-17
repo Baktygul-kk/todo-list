3f1b281c04e7073d41aa614d246126a29d9bfbc2
Baktygul Kozhoshova <87691605+Baktygul-kk@users.noreply.github.com>
2022-02-17 15:35:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-window.addEventListener("load", function(){
+const form = document.getElementById("form");
+const todo = document.getElementById("todo");
+const btn = document.getElementById("btn");
+const item = document.getElementById("todo-item");
 
-    const inputValue = document.getElementById("todo").value;
-    const btn = document.querySelector("button");
-    const toDoItem = document.querySelector("#todo-item");
-    console.log(toDoItem);
-    
 
-    btn.addEventListener("click", function(){
-    const p = document.createElement("p");
-    const userInput =document.createTextNode(inputValue);
-    document.getElementById("todo").value = "";
-    document.getElementById("todo-item").appendChild(p);
-    //input.value = "";
-    p.appendChild(userInput);
-   /* if (inputValue === '') {
-        alert("You must write something!");
-      } else {
-        document.getElementById("todo-item").appendChild(p);
-      }
-      document.getElementById("todo").value = "";*/
-    });
+form.addEventListener("submit", (e)=>{
+e.preventDefault();
 
-    
-      
-      
-   
-
-   
-  
+const input = todo.value;
+console.log(input);
 
 
 
+btn.addEventListener("click", (e)=>{
+  const p = document.createElement("p");
+  item.append(p);
+  p.innerHTML=input;
+ // console.log(p);
+
+ 
+});
+
+item.addEventListener("click", (e)=>{
+  //const clear = item.querySelector("p");
+console.log(clear);
+  clear.remove();
+});
 
 
 
